677a28b61e12c6be8e4bf9b744c57a2789624705
Alan Solis Flores <alansolisflores@outlook.com>
2019-10-10 22:43:29 -0500

    Fragments

--- app/src/main/java/com/alansolisflores/movies/views/MainActivity.java
+++ app/src/main/java/com/alansolisflores/movies/views/MainActivity.java
@@ -7,13 +7,26 @@ import android.widget.Toast;
 
 import com.alansolisflores.movies.R;
 import com.alansolisflores.movies.contracts.MainContract;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity implements MainContract.View {
+
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.initializeProperties();
+    }
+
+    private void initializeProperties(){
+        this.tabLayout = findViewById(R.id.tabLayout);
+        this.tabLayout.addTab(tabLayout.newTab().setText(R.string.popular));
+        this.tabLayout.addTab(tabLayout.newTab().setText(R.string.top_rated));
+        this.tabLayout.addTab(tabLayout.newTab().setText(R.string.upcoming));
+        this.tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        getSupportActionBar().setElevation(0);
     }
 
     @Override
